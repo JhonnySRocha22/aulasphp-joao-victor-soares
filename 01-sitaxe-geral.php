bb40db97b8cb480c9ce2f01ad0b8fa78383253f1
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-26 15:30:11 -0300

    Iniciando PHP

---
+++ 01-sitaxe-geral.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Geral PHP</title>
+</head>
+<body>
+    <h1>Sintaxe Geral - PHP</h1>
+    <hr>
+
+
+    <!-- delimitadores -->
+    <?php
+    echo "<i>Olá PHP!</i>";
+    echo 2023
+    ?>
+
+
+
+
+
+</body>
+</html>
